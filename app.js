e195fb3491ad911803ed643b2e0ce84e1cb2a87b
Rıdvan Şit <99076569+RIDVANSIT@users.noreply.github.com>
2023-11-06 14:53:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const aranacakVeri = document.querySelector("#aranacak_veri");
+const araButtonu = document.querySelector("#ara");
+const sonucYazisi = document.querySelector("#sonuc");
+
+araButtonu.onclick = function() {
+  const aranacakKelime = aranacakVeri.value;
+  console.log(aranacakKelime);
+  const liste = [
+    "elma",
+    "armut",
+    "çilek",
+    "portakal",
+    "muz",
+  ];
+
+  let sonuc = false;
+
+  for (let i = 0; i < liste.length; i++) {
+    if (aranacakKelime === liste[i]) {
+      sonuc = true;
+      break;
+    }
+  }
+
+  if (sonuc) {
+    sonucYazisi.innerHTML = `Aranan kelime bulundu: ${aranacakKelime}`;
+  } else {
+    sonucYazisi.innerHTML = `Aranan kelime bulunamadı.`;
+  }
+};
